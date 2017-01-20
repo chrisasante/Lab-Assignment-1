10c42227b72e40a5def11e091579baad61b8d1ba
Chris Asante <chrisas52@gmail.com>
2017-01-20 19:41:19 +0000

    addition

---
+++ Calc.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Calc
+{
+  public static void main (String[]parameters)
+  {
+    if (parameters[0].equals("add"))
+    {
+      double one = Double.parseDouble(parameters[1]);
+      double two = Double.parseDouble(parameters[2]);
+      System.out.println(one+two);
+    
+    }
+  }
+}
